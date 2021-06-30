b23dc1416b63dbafd7cae43e8a73e3e43d867cdf
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-29 22:03:23 -0300

    Criando a primeira classe

---
+++ index.js
@@ -0,0 +1,15 @@
+//CRIANDO UMA CLASSE
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+//INSTANCIANDO UMA CLASSE
+const cliente1 = new Cliente();
+cliente1.nome="Anne";
+cliente1.cpf=11111111111;
+cliente1.agencia=1001;
+cliente1.saldo=0;
+console.log(cliente1);
---
+++ sem-orientacao-objeto.js
@@ -0,0 +1,15 @@
+const cliente1Nome = "Anne";
+const cliente1CPF = 11111111111;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Joao";
+const cliente2CPF = 22222222222;
+const cliente2Agencia = 1002;
+const cliente2Saldo = 0;
+
+//MUITA REPETIÇÃO...
+console.log(cliente1Nome);
+console.log(cliente2Nome);
+
+//NECESSIDADE DE CRIAR UM "MOLDE"
