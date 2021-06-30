362bfebedf9a28a36a9af6f863c0340026a73aa5
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-06-30 16:05:05 -0300

    Getters e Setters

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -3,6 +3,21 @@ export class ContaCorrente{
     cliente;
     _saldo = 0;
 
+    set cliente(novoValor){
+        if(novaValor instanceof Cliente){
+            this._cliente = novoValor;
+        }  
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+
+
     sacar(valor){
         if(this._saldo >= valor){
             this._saldo -= valor;
--- index.js
+++ index.js
@@ -44,3 +44,10 @@ console.log(conta2);
 //TRANSFERINDO DINHEIRO DA CONTA DA ANNE PARA CONTA 2
 contaCorrenteAnne.transferir(200,conta2);
 console.log(conta2);
+
+//UTILIZANDO GETTERS E SETTERS
+let valor=200;
+contaCorrenteAnne.transferir(valor, conta2)
+//conta2.saldo=3000; -> somente leitura. nao tem como atribuir. 
+//nao possui metodo set, somente get
+console.log(conta2.saldo);
